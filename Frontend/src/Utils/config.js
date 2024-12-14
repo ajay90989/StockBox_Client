c825a185baaf06ed173a89421841e50362f5c5d7
NehaSharma3496 <sharmaneha3496@gmail.com>
2024-12-14 16:39:54 +0530

    Add multiple PDF and CSV files for reports, coupons, and templates

--- Frontend/src/Utils/config.js
+++ Frontend/src/Utils/config.js
@@ -1,9 +1,9 @@
-// export const base_url = `http://localhost:5001/`;
+export const base_url = `http://localhost:5002/`;
 // export const base_url = `http://localhost:5001/`;
 // export const base_url = `http://192.168.0.11:5001/`;
 
 
-export const base_url = `${window.location.origin}/backend/`
+// export const base_url = `${window.location.origin}/backend/`
 
 
 // for image 
